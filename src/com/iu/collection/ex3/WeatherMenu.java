1d04a394c791099a9fd5326616a7bea88ea6b340
LEE BYEONG JU <bj900314@gmail.com>
2019-09-20 17:54:04 +0900

    123

--- src/com/iu/collection/ex3/WeatherMenu.java
+++ src/com/iu/collection/ex3/WeatherMenu.java
@@ -1,21 +1,73 @@
 package com.iu.collection.ex3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.iu.collection.ex2.Student;
+
 public class WeatherMenu {
-	
-	public void start() {
-//		1. 날씨정보 초기화 - init
-//		2. 날씨정보 추가 - addWeather
-//		3. 전체 날씨 정보 출력 - view
-//		4. 날씨 검색 - findWeather
-//		5. 프로그램 종료
-		
-		
-		
-		
-		
-		
-		
+
+	private ArrayList<Weather> wt;
+	private WeatherService ws;
+	private WeatherView wv;
+	private Scanner sc;
+
+	public WeatherMenu() {
+		wt = new ArrayList<Weather>();
+		ws = new WeatherService();
+		wv = new WeatherView();
+		sc = new Scanner(System.in);
 	}
-	
-	
+
+
+	public void start() {
+		//		1. 날씨정보 초기화 - init
+		//		2. 날씨정보 추가 - addWeather
+		//		3. 전체 날씨 정보 출력 - view
+		//		4. 날씨 검색 - findWeather
+		//		5. 프로그램 종료
+
+
+		int select = 0;
+		boolean check = true;
+		while (check) {
+			System.out.println("원하는 메뉴의 숫자를 입력해주세요.");
+			System.out.println("1. 날씨정보 초기화");
+			System.out.println("2. 날씨정보 추가");
+			System.out.println("3. 전체 날씨 정보 출력");
+			System.out.println("4. 날씨 검색");
+			System.out.println("5. 프로그램 종료");
+			select = sc.nextInt();
+			switch (select) {
+			case 1:
+
+				break;
+
+			case 2:
+
+				ws.studentInput(wts);
+				break;
+			case 3:
+
+				wv.view(wts);
+				break;
+			case 4:
+
+				Student stu = ws.studentSearch(wts);
+				wv.view(wt);
+				break;
+			default:
+				System.out.println("프로그램을 종료합니다.");
+				check = !check;
+			}
+		}
+
+
+
+
+
+
+	}
+
+
 }
--- src/com/iu/collection/ex3/WeatherView.java
+++ src/com/iu/collection/ex3/WeatherView.java
@@ -1,16 +1,32 @@
 package com.iu.collection.ex3;
 
+import java.util.ArrayList;
+
 public class WeatherView {
 	
-	public void view() {
+	public void view(ArrayList<Weather> wts) {
 		//날씨 전체 정보 출력
-		
-		
+		System.out.println("전체 날씨의 정보를 조회합니다.");
+		for (int i = 0; i < wts.size(); i++) {
+			System.out.println("도시 이름 : "+wts.get(i).getCity());
+			System.out.println("온도 : "+wts.get(i).getTemperature()+"셀시우스");
+			System.out.println("습도 : "+wts.get(i).getHumidity()+"%");
+			System.out.println("상태 : "+wts.get(i).getStatus());
+			System.out.println("-----------------------");
+		}//for
 
 	}
-	public void view(int v) {
+	public void view(Weather wt) {
 		//날씨 정보 하나 출력
-		
+		if (wt != null) {
+			System.out.println("도시 이름 : "+wt.getCity());
+			System.out.println("온도 : "+wt.getTemperature()+"셀시우스");
+			System.out.println("습도 : "+wt.getHumidity()+"%");
+			System.out.println("상태 : "+wt.getStatus());
+			System.out.println("-----------------------");
+		}else {
+			System.out.println("날씨의 정보가 일치하지않거나 존재하지 않습니다.");
+		}
 	}
 	
 }
